ec2062e49d58f075dc55b90ee9353dbcbc4566eb
Justin Noah <justinnoah@gmail.com>
2015-02-15 23:19:20 -0800

    Add ConnectionResponse packet and encoder method

--- src/parse_packets.rs
+++ src/parse_packets.rs
@@ -12,18 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+extern crate bincode;
+extern crate "rustc-serialize" as rustc_serialize;
 
-#[repr(C)]
+use self::bincode::SizeLimit;
+
 pub struct PacketHeader {
     pub connection_id:  u64,
     pub action:         u32,
     pub transaction_id: u32,
 }
 
+#[derive(RustcEncodable)]
+struct ConnectionResponse {
+    action:         u32,
+    transaction_id: u32,
+    connection_id:  u64,
+}
+
 pub fn parse_header(packet: &[u8]) -> PacketHeader {
-    let p_con_id            = &packet[0..7];
-    let p_action            = &packet[8..11];
-    let p_tran_id           = &packet[12..15];
+    let p_con_id            = &packet[0..8];
+    let p_action            = &packet[8..12];
+    let p_tran_id           = &packet[12..16];
 
     let mut connection_id:  u64 = 0;
     let mut action:         u32 = 0;
@@ -45,4 +55,11 @@ pub fn parse_header(packet: &[u8]) -> PacketHeader {
     }
 
     PacketHeader {connection_id: connection_id, action: action, transaction_id: transaction_id}
+}
+
+pub fn encode_connect_response(uuid: u64, tran_id: u32) -> Vec<u8> {
+    let packet = ConnectionResponse { action: 0, transaction_id: tran_id, connection_id: uuid};
+
+    let encoded = bincode::encode(&packet, SizeLimit::Infinite).unwrap();
+    encoded
 }
