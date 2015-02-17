67bbd9bcf77f49649e11cd820743ca5029a64fc7
Justin Noah <justinnoah@gmail.com>
2015-02-16 23:01:05 -0800

    use bincode to de/encode packet data

--- src/parse_packets.rs
+++ src/parse_packets.rs
@@ -17,6 +17,8 @@ extern crate "rustc-serialize" as rustc_serialize;
 
 use self::bincode::SizeLimit;
 
+
+#[derive(RustcDecodable)]
 pub struct PacketHeader {
     pub connection_id:  u64,
     pub action:         u32,
@@ -31,35 +33,11 @@ struct ConnectionResponse {
 }
 
 pub fn parse_header(packet: &[u8]) -> PacketHeader {
-    let p_con_id            = &packet[0..8];
-    let p_action            = &packet[8..12];
-    let p_tran_id           = &packet[12..16];
-
-    let mut connection_id:  u64 = 0;
-    let mut action:         u32 = 0;
-    let mut transaction_id: u32 = 0;
-
-    for i in p_con_id.iter() {
-        connection_id <<= 8;
-        connection_id |= *i as u64;
-    }
-
-    for i in p_action.iter() {
-        action <<= 8;
-        action |= *i as u32;
-    }
-
-    for i in p_tran_id.iter() {
-        transaction_id <<= 8;
-        transaction_id |= *i as u32;
-    }
-
-    PacketHeader {connection_id: connection_id, action: action, transaction_id: transaction_id}
+    // In case we send extra by mistake, make sure to only parse the first 16 bytes
+    bincode::decode(&packet[..16]).unwrap()
 }
 
 pub fn encode_connect_response(uuid: u64, tran_id: u32) -> Vec<u8> {
     let packet = ConnectionResponse { action: 0, transaction_id: tran_id, connection_id: uuid};
-
-    let encoded = bincode::encode(&packet, SizeLimit::Infinite).unwrap();
-    encoded
+    bincode::encode(&packet, SizeLimit::Infinite).unwrap()
 }
